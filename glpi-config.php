43af08742c26e1f137034a5e373c1aa9027d5a5c
infectiious <kirkwoodjoshua@gmail.com>
2023-04-14 00:30:44 +1000

    Adjusted dockerfile

---
+++ glpi-config.php
@@ -0,0 +1,9 @@
+<?php 
+
+define('GLPI_DB_HOST', 'db'); 
+define('GLPI_DB_PORT', '5432'); 
+define('GLPI_DB_NAME', 'glpi'); 
+define('GLPI_DB_USER', 'glpi_user'); 
+define('GLPI_DB_PASSWORD', 'glpi_password'); 
+
+define('GLPI_TIMEZONE', 'Australia/Sydney');
